2411565ae0e12b1b9de5c1fda7d17e64dd60b86a
Nguyen, Frank <frank.nguyen@thermofisher.com>
2020-04-22 00:00:13 +0800

    feature: add first version of MachineLearning class that provides training / testing / saving / loading a model

---
+++ peltier_sim/machine_learning.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import joblib
+from sklearn.tree import DecisionTreeRegressor
+
+class MachineLearning:
+    def __init__(self):
+        self.accuracy_window = 0    
+        
+    def set_accuracy_window(self, value):
+        self.accuracy_window = value
+    
+    def load_data(self, path):
+        dataset = pd.read_csv(path)        
+        condition = dataset.iloc[:, :-1].values        
+        result = dataset.iloc[:, -1].values        
+    
+        return condition, result
+    
+    def train_model(self, path):
+        train_condition, train_result = self.load_data(path)
+        # Training the model
+        model = DecisionTreeRegressor(random_state=0)
+        model.fit(train_condition, train_result)
+
+        return model
+    
+    def test_model(self, model, path):
+        test_condition, test_result = self.load_data(path)
+        test_prediction = model.predict(test_condition)
+        window = 0.25
+        total = len(test_prediction)
+        correct = 0
+        for i in range(0, total):
+            if test_result[i] - self.accuracy_window <= test_prediction[i] <= test_result[i] + self.accuracy_window:
+                correct += 1
+        print(f"The accuracy of model is {(correct * 100 / total):.2f}")
+    
+    def save_model(self, model, path):
+        # save the model to disk        
+         joblib.dump(model, path)
+
+    def load_model(self, path):
+        # load the model from disk
+        return joblib.load(path)
