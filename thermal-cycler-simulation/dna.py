cf99c18692841f38e90879a14961cedd7c6d0f0b
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-05-08 20:52:17 +0800

    feature: add method blend_in in class DNA
    to assign PID params from DNA object to TBC_Controller PID params

--- thermal-cycler-simulation/dna.py
+++ thermal-cycler-simulation/dna.py
@@ -12,3 +12,10 @@ class DNA:
 
     def random_value(self, min_val, max_val, resolution):        
         return random.randint(int(min_val / resolution), int(max_val / resolution) + 1) * resolution
+
+    def blend_in(self, tbc_controller):
+        tbc_controller.pid.P  = self.genes[0]
+        tbc_controller.pid.I  = self.genes[1]
+        tbc_controller.pid.D  = self.genes[2]
+        tbc_controller.pid.KI = self.genes[3]
+        tbc_controller.pid.KD = self.genes[4]
