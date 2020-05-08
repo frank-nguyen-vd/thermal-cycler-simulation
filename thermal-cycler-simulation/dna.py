ea4f99fcfc2b37dcaedd369453e3e1b76b6178bb
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-05-08 21:21:56 +0800

    mod: rework init() of class DNA

--- thermal-cycler-simulation/dna.py
+++ thermal-cycler-simulation/dna.py
@@ -1,13 +1,10 @@
 import random
 
 class DNA:
-    def __init__(self, pid_specs):
-        self.genes = []
-        self.genes.append(self.random_value(pid_specs.Pmin,  pid_specs.Pmax,  pid_specs.Pres))
-        self.genes.append(self.random_value(pid_specs.IPmin, pid_specs.Imax,  pid_specs.Ires))
-        self.genes.append(self.random_value(pid_specs.Dmin,  pid_specs.Dmax,  pid_specs.Dres))
-        self.genes.append(self.random_value(pid_specs.KImin, pid_specs.KImax, pid_specs.KIres))
-        self.genes.append(self.random_value(pid_specs.KDmin, pid_specs.KDmax, pid_specs.KDres))
+    def __init__(self, pid_specs, dnaLength=5):        
+        self.dnaLength = dnaLength
+        self.genes = self.init_DNA(dnaLength)
+        self.specs = self.save_specs(pid_specs)
         self.score = 0
 
     def random_value(self, min_val, max_val, resolution):        
