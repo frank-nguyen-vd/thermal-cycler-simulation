6590bbe47ea3597a57202c9f21814bdfe82b9d74
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-05-09 23:08:28 +0800

    mod: change the fitness criteria  of all creatures

--- thermal-cycler-simulation/population_manager.py
+++ thermal-cycler-simulation/population_manager.py
@@ -84,216 +84,75 @@ class PopulationManager:
                 hold_time = 35
                 pcr, tbc = self.init_environment(block_temp=setpoint1)
                 creature.blend_in(tbc)
-                creature.score = 0                
-
+                creature.score = 0
                 tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)
-                time_limit = 10 * (setpoint2 - setpoint1) / tbc.max_up_ramp
-                sys_time = 0
+                time_limit = 2 * (setpoint2 - setpoint1) / tbc.max_up_ramp
                 ctime = 0
-                start_temp = setpoint1
-                while True:
-                    if tbc.stage != "Ramp Up":
-                        avg_rate = (pcr.sample_temp - start_temp) / ctime
-                        creature.score -= abs((avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp) * 100
-                        break
-                    
-                    if sys_time > time_limit:
+                up_deviation = 0
+                while tbc.stage != "Hold":
+                    if ctime > time_limit:
                         creature.alive = False
                         break
-
                     tbc.tick(0.05)
                     pcr.tick(0.05)
                     ctime += 0.05
-                    sys_time += 0.05
+                    if pcr.sample_temp - setpoint2 > up_deviation:
+                        up_deviation = pcr.sample_temp - setpoint2                    
 
-                if tbc.stage == "Overshoot Over" and creature.alive:
-                    start_temp = pcr.sample_temp
-                    ctime = 0
-                    while True:
-                        if tbc.stage != "Overshoot Over":
-                            avg_rate = (pcr.sample_temp - start_temp) / ctime
-                            creature.score -= abs((avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp) * 100
-                            break
-
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)
-                        ctime += 0.05
-                        sys_time += 0.05                    
-
-                if tbc.stage == "Hold Over" and creature.alive:
-                    hold_temp = tbc.pid2.SP * 2
-                    min_deviation = 0
-                    max_deviation = 0                    
-                    while True:
-                        if tbc.stage != "Hold Over":
-                            if max_deviation > 0.25:
-                                creature.score -= (max_deviation - 0.25) * 10
-                            if min_deviation > 0.25:
-                                creature.score -= (min_deviation - 0.25) * 10
-                            break
-                            
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)  
-                        if pcr.block_temp - hold_temp > max_deviation:
-                            max_deviation = pcr.block_temp - hold_temp
-                        elif hold_temp - pcr.block_temp > min_deviation:
-                            min_deviation = hold_temp - pcr.block_temp                     
-                        sys_time += 0.05     
-
-                if tbc.stage == "Land Over" and creature.alive:
-                    start_temp = pcr.block_temp
-                    sample_overshoot = 0                    
-                    ctime = 0
-                    while True:
-                        if tbc.stage != "Land Over":
-                            avg_rate = abs(pcr.block_temp - start_temp) / ctime
-                            creature.score += avg_rate / 2
-                            creature.score -= sample_overshoot * 20
-                            break
-                        
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)
-                        if pcr.sample_temp - setpoint2 > sample_overshoot:
-                            sample_overshoot = pcr.sample_temp - setpoint2
-                        ctime += 0.05
-                        sys_time += 0.05                         
-
-                # tbc_controller must reach stage "Hold" at this point
                 if not creature.alive:
-                    creature.score -= 200
+                    creature.score -= 100
                     # giving the creature second chance to live
-                    creature.alive = True
-                    setpoint1 = 60
-                    setpoint2 = 95
-                    hold_time = 35
+                    creature.alive = True                    
                     pcr, tbc = self.init_environment(block_temp=setpoint2)
-                    tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)
                     creature.blend_in(tbc)
+                    tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)                    
                 else:
-                    avg_rate = (pcr.sample_temp - setpoint1) / sys_time                
-                    creature.score -= abs((avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp) * 200
+                    avg_rate = (pcr.sample_temp - setpoint1) / ctime
+                    creature.score -= abs(avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp * 100
+                    if up_deviation > 0.25:
+                        creature.score -= up_deviation * 50
 
-                max_deviation = 0
-                min_deviation = 0
-                hold_temp = setpoint2                
-                time_limit = hold_time                
+                up_deviation = 0
+                down_deviation = 0
                 ctime = 0
-                while True:
-                    if tbc.stage != "Hold" or ctime > time_limit:
-                        if max_deviation > 0.1:
-                            creature.score -= (max_deviation - 0.1) * 30
-                        if min_deviation > 0.1:
-                            creature.score -= (min_deviation - 0.1) * 30
-                        break
-
+                while ctime < hold_time:
+                    if tbc.stage != "Hold":
+                        print("ERROR: TBC Controller is not at stage HOLD while holding at setpoint")
+                        raise Exception
                     tbc.tick(0.05)
                     pcr.tick(0.05)
-                    if pcr.sample_temp - hold_temp > max_deviation:
-                        max_deviation = pcr.sample_temp - hold_temp
-                    elif hold_temp - pcr.sample_temp > min_deviation:
-                        min_deviation = hold_temp - pcr.sample_temp
                     ctime += 0.05
-
+                    if pcr.sample_temp - setpoint2 > up_deviation:
+                        up_deviation = pcr.sample_temp - setpoint2
+                    elif setpoint2 - pcr.sample_temp > down_deviation:
+                        down_deviation = setpoint2 - pcr.sample_temp
+                    
+                if up_deviation > 0.1:
+                    creature.score -= (up_deviation - 0.1) * 50
+                if down_deviation > 0.1:
+                    creature.score -= (down_deviation - 0.1) * 50
+                
                 tbc.ramp_to(new_set_point=setpoint1, sample_rate=100)
-                time_limit = 10 * abs(setpoint2 - setpoint1) / tbc.max_down_ramp
-                sys_time = 0
+                time_limit = 2 * (setpoint2 - setpoint1) / tbc.max_down_ramp
                 ctime = 0
-                start_temp = setpoint2
-                while True:
-                    if tbc.stage != "Ramp Down":
-                        avg_rate = abs(pcr.sample_temp - start_temp) / ctime
-                        creature.score -= abs((avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp) * 100
-                        break
-                    if sys_time > time_limit:
+                down_deviation = 0
+                while tbc.stage != "Hold":
+                    if ctime > time_limit:
                         creature.alive = False
                         break
                     tbc.tick(0.05)
                     pcr.tick(0.05)
                     ctime += 0.05
-                    sys_time += 0.05                    
+                    if setpoint1 - pcr.sample_temp > down_deviation:
+                        down_deviation = setpoint1 - pcr.sample_temp
 
-                if tbc.stage == "Overshoot Under" and creature.alive:
-                    start_temp = pcr.sample_temp
-                    ctime = 0
-                    while True:
-                        if tbc.stage != "Overshoot Under":
-                            avg_rate = abs(pcr.sample_temp - start_temp) / ctime
-                            creature.score -= abs((avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp) * 100
-                            break
-
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)
-                        ctime += 0.05
-                        sys_time += 0.05                    
-
-                if tbc.stage == "Hold Under" and creature.alive:
-                    hold_temp = tbc.pid2.SP * 2
-                    min_deviation = 0
-                    max_deviation = 0                    
-                    while True:
-                        if tbc.stage != "Hold Under":
-                            if max_deviation > 0.25:
-                                creature.score -= (max_deviation - 0.25) * 10
-                            if min_deviation > 0.25:
-                                creature.score -= (min_deviation - 0.25) * 10
-                            break
-                            
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)  
-                        if pcr.block_temp - hold_temp > max_deviation:
-                            max_deviation = pcr.block_temp - hold_temp
-                        elif hold_temp - pcr.block_temp > min_deviation:
-                            min_deviation = hold_temp - pcr.block_temp                     
-                        sys_time += 0.05        
-
-                if tbc.stage == "Land Under" and creature.alive:
-                    start_temp = pcr.block_temp
-                    sample_overshoot = 0                    
-                    ctime = 0
-                    while True:
-                        if tbc.stage != "Land Under":
-                            avg_rate = abs(pcr.block_temp - start_temp) / ctime
-                            creature.score += avg_rate / 2                            
-                            creature.score -= sample_overshoot * 20
-                            break
-
-                        if sys_time > time_limit:
-                            creature.alive = False
-                            break
-
-                        tbc.tick(0.05)
-                        pcr.tick(0.05)
-                        if setpoint1 - pcr.sample_temp > sample_overshoot:
-                            sample_overshoot = setpoint1 - pcr.sample_temp
-                        ctime += 0.05
-                        sys_time += 0.05         
-
-                # tbc_controller must reach stage "Hold" at this point
                 if not creature.alive:
-                    creature.score -= 200
+                    creature.score -= 100
                 else:
-                    avg_rate = abs(pcr.sample_temp - setpoint2) / sys_time                
-                    creature.score -= abs((avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp) * 200
+                    avg_rate = (setpoint2 - pcr.sample_temp) / ctime
+                    creature.score -= abs(avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp * 100
+                    if down_deviation > 0.25:
+                        creature.score -= down_deviation * 50
 
                 print(f"Creature {loc} scores {creature.score}")
 
