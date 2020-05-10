e671fc211bb46e4783de59fb7e2261e57d4da7ad
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-05-10 11:31:25 +0800

    mod: create new methods to evaluate creature's fitness, export creature's PID params and protocol run data

--- thermal-cycler-simulation/population_manager.py
+++ thermal-cycler-simulation/population_manager.py
@@ -75,6 +75,9 @@ class PopulationManager:
 
 
     def breed_population(self, population):
+        if population == []:
+            return self.create_population(self.pop_size)
+
         if len(population) <= 1:
             print("ERROR: Only one creature left. Population is dead.")
             raise Exception
@@ -97,118 +100,148 @@ class PopulationManager:
         new_pop.append(self.create_genius())
         return new_pop
 
-    def run(self):
-        population = self.create_population(self.pop_size)        
-        for i in range(0, self.max_generation):
-            print(f"-------- Generation={i} Population={len(population)}")
-            for loc, creature in enumerate(population):
-                
-                setpoint1 = 60
-                setpoint2 = 95
-                hold_time = 35
-                pcr, tbc = self.init_environment(block_temp=setpoint1)
-                creature.blend_in(tbc)
-                creature.score = 0
-                tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)
-                time_limit = 2.5 * (setpoint2 - setpoint1) / tbc.max_up_ramp
-                ctime = 0
-                up_deviation = 0
-                while tbc.stage != "Hold":
-                    if ctime > time_limit:
-                        creature.alive = False
-                        break
-                    tbc.tick(0.05)
-                    pcr.tick(0.05)
-                    ctime += 0.05
-                    if pcr.sample_temp - setpoint2 > up_deviation:
-                        up_deviation = pcr.sample_temp - setpoint2                    
-
-                if not creature.alive:
-                    creature.score -= 1000
-                    # giving the creature second chance to live
-                    creature.alive = True                    
-                    pcr, tbc = self.init_environment(block_temp=setpoint2)
-                    creature.blend_in(tbc)
-                    tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)                    
-                else:
-                    avg_rate = (pcr.sample_temp - setpoint1) / ctime
-                    creature.score -= abs(avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp * 100
-                    if up_deviation > 0.25:
-                        creature.score -= up_deviation * 50
-
-                up_deviation = 0
-                down_deviation = 0
-                ctime = 0
-                while ctime < hold_time:
-                    if tbc.stage != "Hold":
-                        print("ERROR: TBC Controller is not at stage HOLD while holding at setpoint")
-                        raise Exception
-                    tbc.tick(0.05)
-                    pcr.tick(0.05)
-                    ctime += 0.05
-                    if pcr.sample_temp - setpoint2 > up_deviation:
-                        up_deviation = pcr.sample_temp - setpoint2
-                    elif setpoint2 - pcr.sample_temp > down_deviation:
-                        down_deviation = setpoint2 - pcr.sample_temp
-                    
-                if up_deviation > 0.1:
-                    creature.score -= (up_deviation - 0.1) * 50
-                if down_deviation > 0.1:
-                    creature.score -= (down_deviation - 0.1) * 50
-                
-                tbc.ramp_to(new_set_point=setpoint1, sample_rate=100)
-                time_limit = 2.5 * (setpoint2 - setpoint1) / tbc.max_down_ramp
-                ctime = 0
-                down_deviation = 0
-                while tbc.stage != "Hold":
-                    if ctime > time_limit:
-                        creature.alive = False
-                        break
-                    tbc.tick(0.05)
-                    pcr.tick(0.05)
-                    ctime += 0.05
-                    if setpoint1 - pcr.sample_temp > down_deviation:
-                        down_deviation = setpoint1 - pcr.sample_temp
-
-                if not creature.alive:
-                    creature.score -= 1000
-                else:
-                    avg_rate = (setpoint2 - pcr.sample_temp) / ctime
-                    creature.score -= abs(avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp * 100
-                    if down_deviation > 0.25:
-                        creature.score -= down_deviation * 50
-
-                print(f"Creature {loc} scores {creature.score} fitness points")
-
-            population.sort(key=self.getScore, reverse=True)
-            if i + 1 >= self.max_generation:
+    def eval_fitness_score(self, creature):
+        setpoint1 = 60
+        setpoint2 = 95
+        hold_time = 35
+        pcr, tbc = self.init_environment(block_temp=setpoint1)
+        creature.blend_in(tbc)
+        creature.score = 0
+        tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)
+        time_limit = 2.5 * (setpoint2 - setpoint1) / tbc.max_up_ramp
+        ctime = 0
+        up_deviation = 0
+        while tbc.stage != "Hold":
+            if ctime > time_limit:
+                creature.alive = False
                 break
-            for i in range(0, self.pop_size // 2):
-                population.pop()
-            population = self.breed_population(population)
+            tbc.tick(0.05)
+            pcr.tick(0.05)
+            ctime += 0.05
+            if pcr.sample_temp - setpoint2 > up_deviation:
+                up_deviation = pcr.sample_temp - setpoint2                    
 
-        population.sort(key=self.getScore, reverse=True)
-        print("==============================================================")
-        print(f"Best score is {population[0].score} fitness points")
-        k = 0
-        for group_index in range(0, 9):
-            P  = population[0].genes[k]
-            I  = population[0].genes[k + 1]
-            D  = population[0].genes[k + 2]
-            KI = population[0].genes[k + 3]
-            KD = population[0].genes[k + 4]            
-            k += 5
-            print(f"Group {group_index}: P={P} I={I} D={D} KI={KI} KD={KD}")
+        if not creature.alive:
+            creature.score -= 1000
+            # giving the creature second chance to live
+            creature.alive = True                    
+            pcr, tbc = self.init_environment(block_temp=setpoint2)
+            creature.blend_in(tbc)
+            tbc.ramp_to(new_set_point=setpoint2, sample_rate=100)                    
+        else:
+            avg_rate = (pcr.sample_temp - setpoint1) / ctime
+            creature.score -= abs(avg_rate - tbc.max_up_ramp) / tbc.max_up_ramp * 100
+            if up_deviation > 0.25:
+                creature.score -= up_deviation * 50
+
+        up_deviation = 0
+        down_deviation = 0
+        ctime = 0
+        while ctime < hold_time:
+            if tbc.stage != "Hold":
+                print("ERROR: TBC Controller is not at stage HOLD while holding at setpoint")
+                raise Exception
+            tbc.tick(0.05)
+            pcr.tick(0.05)
+            ctime += 0.05
+            if pcr.sample_temp - setpoint2 > up_deviation:
+                up_deviation = pcr.sample_temp - setpoint2
+            elif setpoint2 - pcr.sample_temp > down_deviation:
+                down_deviation = setpoint2 - pcr.sample_temp
+            
+        if up_deviation > 0.1:
+            creature.score -= (up_deviation - 0.1) * 50
+        if down_deviation > 0.1:
+            creature.score -= (down_deviation - 0.1) * 50
+        
+        tbc.ramp_to(new_set_point=setpoint1, sample_rate=100)
+        time_limit = 2.5 * (setpoint2 - setpoint1) / tbc.max_down_ramp
+        ctime = 0
+        down_deviation = 0
+        while tbc.stage != "Hold":
+            if ctime > time_limit:
+                creature.alive = False
+                break
+            tbc.tick(0.05)
+            pcr.tick(0.05)
+            ctime += 0.05
+            if setpoint1 - pcr.sample_temp > down_deviation:
+                down_deviation = setpoint1 - pcr.sample_temp
+
+        if not creature.alive:
+            creature.score -= 1000
+        else:
+            avg_rate = (setpoint2 - pcr.sample_temp) / ctime
+            creature.score -= abs(avg_rate - tbc.max_down_ramp) / tbc.max_down_ramp * 100
+            if down_deviation > 0.25:
+                creature.score -= down_deviation * 50
+
+    def export_creature(self, creature, filepath):
+        import csv
+        stage = [  "Ramp Up", 
+                        "Overshoot Over", 
+                        "Hold Over", 
+                        "Land Over", 
+                        "Hold", 
+                        "Ramp Down", 
+                        "Overshoot Under", 
+                        "Hold Under", 
+                        "Land Under"
+                    ]
+        with open(filepath, 'w', newline='') as file:
+            writer = csv.writer(file)            
+            writer.writerow(["Score"])
+            writer.writerow([creature.score])            
+            writer.writerow(["Stage", "P", "I", "D", "KI", "KD"])
+            for k in range(0, 9):
+                P  = creature.genes[5 * k]
+                I  = creature.genes[5 * k + 1]
+                D  = creature.genes[5 * k + 2]
+                KI = creature.genes[5 * k + 3]
+                KD = creature.genes[5 * k + 4]                                
+                writer.writerow([stage[k], P, I, D, KI, KD])
+
         protocol = Protocol(listSP   =[ 95,  60], 
                             listRate =[100, 100], 
                             listHold =[ 35,  35], 
                             nCycles  =1, 
                             Tblock   =60, 
                             Tamb     =25,
-                            record_filepath=self.record_filepath
+                            record_filepath=filepath
                             )
-        population[0].blend_in(protocol.tbc_controller)
-        protocol.run()            
+        creature.blend_in(protocol.tbc_controller)
+        protocol.run()  
+
+    def run(self):
+        population = []
+        for noGeneration in range(0, self.max_generation):
+            population = self.breed_population(population)
+            print(f"-------- Generation={noGeneration} Population={len(population)}")
+            for loc, creature in enumerate(population):
+                self.eval_fitness_score(creature)
+                print(f"Creature {loc} scores {creature.score} fitness points")
+
+            population.sort(key=self.getScore, reverse=True)
+            for i in range(0, self.pop_size // 2):
+                population.pop()
+            
+
+        population.sort(key=self.getScore, reverse=True)
+        print("==============================================================")
+        print(f"The best score is {population[0].score} fitness points")
+        group_name = ["Ramp Up", "Overshoot Over", "Hold Over", "Land Over", "Hold", "Ramp Down", "Overshoot Under", "Hold Under", "Land Under"]
+        for k in range(0, 9):
+            P  = population[0].genes[5 * k]
+            I  = population[0].genes[5 * k + 1]
+            D  = population[0].genes[5 * k + 2]
+            KI = population[0].genes[5 * k + 3]
+            KD = population[0].genes[5 * k + 4]                
+            print(f"Stage {group_name[k]}: P={P} I={I} D={D} KI={KI} KD={KD}")
+            
+        self.export_creature(population[0], self.record_filepath)
+        
+    
+          
 
 if __name__ == "__main__":
     popMan = PopulationManager(max_generation=200, pop_size=100, mutation_chance=0.01, record_filepath="genius_creature.csv")
