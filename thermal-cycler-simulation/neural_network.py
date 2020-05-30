8d9bf62e735c12d1d007c743fe25102f0a1eb196
Frank Nguyen <frank.nguyen.vd@gmail.com>
2020-05-30 21:40:07 +0800

    Tune the neural network

--- thermal-cycler-simulation/neural_network.py
+++ thermal-cycler-simulation/neural_network.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer, PowerTransformer
 from sklearn.pipeline import Pipeline
 from keras.models import load_model
 import joblib
@@ -29,15 +29,16 @@ class MachineLearning:
         # define base model
         def baseline_model():
             model = Sequential()
-            model.add(Dense(13, input_dim=5, kernel_initializer='normal', activation='relu'))
-            model.add(Dense(1, kernel_initializer='normal'))
-            model.compile(loss='mean_squared_error', optimizer='adam')
+            model.add(Dense(1000, input_dim=5, kernel_initializer='truncated_normal', activation='relu'))
+            model.add(Dropout(0.1))
+            model.add(Dense(1, kernel_initializer='truncated_normal'))
+            model.compile(loss='mean_squared_error', optimizer='sgd')
             return model
-        X_train, y_train = self.load_data(train_path)        
-        # evaluate model with standardized dataset
+        X_train, y_train = self.load_data(train_path)
+        # evaluate model with scalerd dataset
         pipeline = []
-        pipeline.append(('standardize', StandardScaler()))
-        pipeline.append(('estimator', KerasRegressor(build_fn=baseline_model, epochs=25, batch_size=10, verbose=0)))
+        pipeline.append(('scaler', StandardScaler()))
+        pipeline.append(('estimator', KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=20, verbose=1)))
         pipeline = Pipeline(pipeline)
         pipeline.fit(X_train, y_train)
         
@@ -45,35 +46,35 @@ class MachineLearning:
     
     def test_model(self, regressor, test_path=None, acc_win=0.1,):
         
-        X_test, y_true = self.load_data(test_path)        
+        X_test, y_true = self.load_data(test_path)
         y_pred = regressor.predict(X_test)
 
         total = len(y_true)
         correct = 0
         for i in range(0, total):
-            if abs(y_true[i] - y_pred[i]) <= acc_win:               
-                correct += 1            
+            if abs(y_true[i] - y_pred[i]) <= acc_win:  
+                correct += 1
         score = correct / total * 100
         return score
     
     def save_model(self, pipeline, folder='pcr_model'):
         os.makedirs(folder, exist_ok=True)
-        pickle.dump(pipeline.named_steps['standardize'], open(f'{folder}/standardize.pkl', 'wb'))    
+        pickle.dump(pipeline.named_steps['scaler'], open(f'{folder}/scaler.pkl', 'wb'))    
         pipeline.named_steps['estimator'].model.save(f'{folder}/estimator.h5')
 
     def load_model(self, folder='pcr_model'):
-        standardize = pickle.load(open(f'{folder}/standardize.pkl','rb'))    
+        scaler = pickle.load(open(f'{folder}/scaler.pkl','rb'))    
         build_model = lambda: x
         regressor = KerasRegressor(build_fn=build_model, epochs=1, batch_size=10, verbose=0)
         regressor.model = load_model(f'{folder}/estimator.h5')
-        return Pipeline([('standardize', standardize), ('estimator', regressor)])
+        return Pipeline([('scaler', scaler), ('estimator', regressor)])
 
     
 if __name__ == "__main__":
     learning = MachineLearning()
     pipeline = learning.train_model(train_path="train/pcr_training_set.csv")
     score = learning.test_model(pipeline, test_path="test/pcr_testing_set.csv", acc_win=0.1)
-    print("Accuracy = {score:.2f}%")
+    print(f"Accuracy = {score:.2f}%")
     
     learning.save_model(pipeline)
     pipeline = learning.load_model()
